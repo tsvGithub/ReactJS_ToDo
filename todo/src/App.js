6d19c2f43c6da619b4a19a2438ba97a01c7da5d2
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-06-15 14:34:10 +0300

    first commit

---
+++ todo/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="todo">
+      <div className="todo__sidebar">
+        <button>Hello!</button>
+      </div>
+      <div className="todo__tasks"></div>
+    </div>
+  );
+}
+
+export default App;
